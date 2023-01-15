b43219bfa6f3add2cc5d515737ecdf18de681a80
andrebraga7 <andrestrevisan@gmail.com>
2023-01-15 18:04:31 +0000

    Post details page

--- penso_logo_desisto/urls.py
+++ penso_logo_desisto/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import PostList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('blog.urls'), name='blog_urls'),
+    path('accounts/', include('allauth.urls')),
+
 ]
